ee51e46f25e06f25a8d165d3d4844eac16d6a3ee
Gilles Le Bris <lebrisg@hotmail.com>
2018-03-10 23:11:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,21 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
+var initGame = function () {
+    var cfg = {
+        draggable: true,
+        position: 'start',
+        onDrop: handleMove,
+    };
+
+    board = new ChessBoard('gameBoard', cfg);
+    game = new Chess();
+}
+
+var handleMove = function(source, target) {
+    var move = game.move({from: source, to: target});
+}
+
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
