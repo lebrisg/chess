7ccaf3b9fad04cecb97b961d772420320e5c568d
Gilles Le Bris <lebrisg@hotmail.com>
2018-03-10 23:34:08 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 //  OpenShift sample Node application
 var express = require('express'),
     app     = express(),
-    morgan  = require('morgan');
+    morgan  = require('morgan'),
+    chessboard = require('chessboard');
     
 Object.assign=require('object-assign')
 
