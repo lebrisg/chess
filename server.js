db3244ed526a6fb0de03063351a81ab525432772
Gilles Le Bris <lebrisg@hotmail.com>
2018-03-10 23:48:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
-var board = ChessBoard('pgn');
+var board = ChessBoard('pgn','start');
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
