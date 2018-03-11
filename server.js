f6d892f3852713b317be658cbfdeb94bde8faa3f
Gilles Le Bris <lebrisg@hotmail.com>
2018-03-11 09:43:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ var http = require('http').Server(app);
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
+app.get('/', function(req, res) {
+ res.sendFile(__dirname + '/public/default.html');
+});
+
 // error handling
 app.use(function(err, req, res, next){
   console.error(err.stack);
