9d8164861c394872b941c5606bb0755302810a9d
Gilles Le Bris <lebrisg@hotmail.com>
2018-03-11 09:56:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.get('/', function(req, res) {
- res.sendFile(__dirname + '/public/default.html');
+ res.sendFile(__dirname + '/public/index.html');
 });
 
 // error handling
