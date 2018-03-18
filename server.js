d298ea82fd4b0eb237ee004e8822e73ff9d77658
Gilles Le Bris <lebrisg@hotmail.com>
2018-03-18 09:49:24 +0100

    Update index.html

--- server.js
+++ server.js
@@ -1,28 +1,14 @@
 //  OpenShift sample Node application
-var express = require('express'),
-    app     = express(),
-    board   = require('chessboard');
+var http  = require('http'),
+    board = require('chessboard');
     
-app.use(express.static('public'));
-var http = require('http').Server(app);
-
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-var chessboard = ChessBoard('board');
+http.createServer(function(req, res){
+	res.writeHead(200, { 'Content-Type': 'text/html' });
+        var chessboard = chessboard('board');
+	res.end(__dirname + index.html);
+}).listen(port);
 
-app.get('/', function(req, res) {
- res.sendFile(__dirname + '/public/index.html');
-});
-
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
-
-app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
-
-module.exports = app ;
-
---
+++ server3.js
@@ -0,0 +1,28 @@
+//  OpenShift sample Node application
+var express = require('express'),
+    app     = express(),
+    board   = require('chessboard');
+    
+app.use(express.static('public'));
+var http = require('http').Server(app);
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+var chessboard = ChessBoard('board');
+
+app.get('/', function(req, res) {
+ res.sendFile(__dirname + '/public/index.html');
+});
+
+// error handling
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
+});
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app ;
+
