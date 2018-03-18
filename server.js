61fcdf085dcb7a48f69051a11b9f5bcb40c36e83
Gilles Le Bris <lebrisg@hotmail.com>
2018-03-18 09:55:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ var http  = require('http'),
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
+if (typeof document === 'undefined') {
+  global.document = {
+    createElement: () => null,
+  };
+}
+
 http.createServer(function(req, res){
 	res.writeHead(200, { 'Content-Type': 'text/html' });
         var chessboard = chessboard('board');
