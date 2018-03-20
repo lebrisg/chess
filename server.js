51776154c01edfadfc14f800626b3bce02f4e54b
Gilles Le Bris <lebrisg@hotmail.com>
2018-03-20 13:21:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 //  OpenShift sample Node application
-var http  = require('http'),
-    board = require('chessboard');
+var http  = require('http');
     
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
