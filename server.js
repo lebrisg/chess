8c7e1e9d8fb99d4623ce60e9e61c2799ec63e2fe
Gilles Le Bris <lebrisg@hotmail.com>
2018-03-20 13:24:36 +0100

    Fix typo

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 
 http.createServer(function(req, res){
 	res.writeHead(200, { 'Content-Type': 'text/html' });
-	res.end(__dirname + index.html);
+	res.end(__dirname + "index.html");
 }).listen(port);
 
 console.log('Server running on http://%s:%s', ip, port);
