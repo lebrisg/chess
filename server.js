c1b95a587084f5f4e18c03d1afb777151a6a9a03
Gilles Le Bris <lebrisg@hotmail.com>
2018-03-28 14:04:49 +0200

    Test

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 http.createServer(function(req, res){
-	console.log('Path: %s', __dirname + '/index.html' );
 	fs.readFile(__dirname + '/index.html', function(err, data){
 		if(err){
 			res.writeHead(500, { 'Content-Type': 'text/html' });
