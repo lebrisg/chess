3a4664401faddc0138bb61801e6dd862a6077d00
Gilles Le Bris <lebrisg@hotmail.com>
2018-03-28 13:25:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 http.createServer(function(req, res){
-	res.writeHead(200, { 'Content-Type': 'text/html' });
-	res.end(__dirname + "index.html");
+	fs.readFile(__dirname + 'index.html', function(err, data){
+		if(err){
+			res.writeHead(500, { 'Content-Type': 'text/html' });
+			res.end('<html>500 - Internal Error</html>');
+		} else {
+			res.writeHead(200, { 'Content-Type': 'text/html' });
+			res.end(data);
+		}
+	});
 }).listen(port);
 
 console.log('Server running on http://%s:%s', ip, port);
